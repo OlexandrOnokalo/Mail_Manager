0b3a9d654571989141b03e84edfa64a2c5493e73
vynog_vm19@student.itstep.org <vynog_vm19@student.itstep.org>
2025-11-03 17:30:01 +0200

    added login window code

--- Mail_Manager/Windows/LoginWindow.xaml.cs
+++ Mail_Manager/Windows/LoginWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Mail_Manager
 {
@@ -19,9 +7,21 @@ namespace Mail_Manager
     /// </summary>
     public partial class LoginWindow : Window
     {
+        public string? TextEmail { get; set; }
+        public string? TextPassword { get; set; }
         public LoginWindow()
         {
             InitializeComponent();
+        }
+        private void ToConfirm(object sender, RoutedEventArgs e)
+        {
+            if (Email.Text == "lenailyshun@gmail.com" && Password.Text == "hadv dehd bsdf ocye")
+            {
+                TextEmail = Email.Text;
+                TextPassword = Password.Text;
+                Close();
+            }
+            else { MessageBox.Show("Incorrect email address or password"); }
         }
     }
 }
--- Mail_Manager/Windows/MainWindow.xaml.cs
+++ Mail_Manager/Windows/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Mail_Manager
 {
@@ -16,15 +7,16 @@ namespace Mail_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
+        string? email;
+        string? password;
         public MainWindow()
         {
             InitializeComponent();
-            
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("hello");
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+            email = loginWindow.TextEmail;
+            password = loginWindow.TextPassword;
+            if (email == null || password == null) { Close(); }
         }
     }
 }
