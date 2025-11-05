b5520a611666a913182291ca3cc02fb37610677e
Artem <ademchuck09@gmail.com>
2025-11-05 17:29:20 +0200

    create view mesege window

---
+++ Mail_Manager/Windows/ViewMessageWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mail_Manager.Windows
+{
+    /// <summary>
+    /// Interaction logic for ViewMessageWindow.xaml
+    /// </summary>
+    public partial class ViewMessageWindow : Window
+    {
+        public ViewMessageWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
